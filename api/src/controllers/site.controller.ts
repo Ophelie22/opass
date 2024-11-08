eb99ca16c5c9beb4d8c103a8c8a7976b56dab989
BilalYildiz51 <ydzbilal51@gmail.com>
2024-11-08 14:46:00 +0100

    Feat|Fix|Perf: Ajout des 9 tables avec correction sur les relations surtout sur les deux cles primaires composites, améliorations de la gestion des erreurs pour tout les CRUD

--- api/src/controllers/site.controller.ts
+++ api/src/controllers/site.controller.ts
@@ -4,12 +4,45 @@ import { PrismaClient } from '@prisma/client';
 const prismaClient = new PrismaClient();
 
 
+// Récuperer tous les sites
+
+export const getAllSites = async (req: Request, res: Response) => {
+    try {
+        const sites = await prisma.sites.findMany({
+            include: { region: true, category: true, events: true, siteUsers: true, sitePackage: true },
+        });
+        res.status(200).json({ data: sites})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur lors de la récupération des sites"});
+    }
+};
+
+// Récuperer un site par ID
+
+export const getSiteById = async (req: Request, res: Response) => {
+    try {
+        const siteId = parseInt(req.params.id, 10);
+        const site = await prisma.site.findUnique({
+            where: { id : siteId },
+            include: { region: true, category: true, events: true, siteUsers: true, sitePackage: true },
+        })
+        if (!site) {
+            return res.status(404).json({ message: "Site non trouvé"});
+        }
+        res.status(200).json({ data: site});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur lors de la récupération du site" });
+    }
+};
+
 // Creation d'un site
-export const createSite = async (req: Request, res: Response): Promise<void> => {
+export const createSite = async (req: Request, res: Response):  => {
     try {
         const { regionId, categoryId, name, description, city, postalCode, address, latitude, longitude, media, contact, information } = req.body;
 
-        const newSite = await prismaClient.site.create({
+        const newSite = await prisma.site.create({
             data: {
                 regionId,
                 categoryId,
@@ -25,8 +58,55 @@ export const createSite = async (req: Request, res: Response): Promise<void> => 
                 information,
             },
         });
-        res.status(201).json(newSite);
-    } catch (error: any) {
-        res.status(500).json({ error: 'Site creation failed', details: error.message });
+        res.status(201).json({ data: newSite});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Erreur lors de la création du site" });
+    }
+};
+
+// Mettre à jour un site par ID
+
+export const updateSiteById = async (req: Request, res: Response) => {
+    try {
+        const siteId = parseInt(req.params.id, 10);
+        const { regionId, categoryId, name, description, city, postalCode, address, latitude, longitude, media, contact, information } = req.body;
+
+        const updatedSite = await prisma.site.update({
+            where: { id: siteId },
+            data: {
+                regionId,
+                categoryId,
+                name,
+                description,
+                city,
+                postalCode,
+                address,
+                latitude,
+                longitude,
+                media,
+                contact,
+                information,
+            },
+        });
+        res.status(200).json({ data: updatedSite });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur lors de la mise à jour du site" });
+    }
+};
+
+
+// Supprimer un site par ID
+export const deleteSiteById = async (req: Request, res: Response) => {
+    try {
+        const siteId = parseInt(req.params.id, 10);
+        await prisma.site.delete({
+            where: { id: siteId },
+        });
+        res.status(200).json({ message: "Site supprimé avec succès" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erreur lors de la suppression du site" });
     }
 };
--- api/src/routes/orderRoutes.ts
+++ api/src/routes/orderRoutes.ts
@@ -36,4 +36,60 @@ app.get('/orders', async (req, res) => {
         res.status(500).json({ error: 'Failed to retrieve orders', details: error.message });
     }
 });
+
 //Lire une seule commande
+app.get('/orders/:id', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const order = await prismaClient.order.findUnique({
+            where: { id: parseInt(id) },
+            include: { user: true, Passes: true },
+        });
+
+        if (!order) {
+            res.status(404).json({ error: 'Order not found' });
+            return;
+        }
+        res.status(200).json(order);
+    } catch (error: any) {
+        res.status(500).json({ error: 'Failed to retrieve order', details: error.message });
+    }
+});
+
+// Mise à jour des Commandes
+app.put('/orders/:id', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        const { userId, amount, status } = req.body;
+
+        const updatedOrder = await prismaClient.order.update({
+            where: { id: parseInt(id) },
+            data: {
+                userId,
+                amount,
+                status,
+            },
+        });
+
+        res.status(200).json(updatedOrder);
+    } catch (error: any) {
+        res.status(500).json({ error: 'Order update failed', details: error.message });
+    }
+});
+// Supprimer une commande
+app.delete('/orders/:id', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { id } = req.params;
+        await prismaClient.order.delete({
+            where: { id: parseInt(id) },
+        });
+        res.status(204).send();
+    } catch (error: any) {
+        res.status(500).json({ error: 'Order deletion failed', details: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
