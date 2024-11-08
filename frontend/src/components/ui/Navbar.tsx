c107243ba3a89b12b538ef87531754e1ec9b3f0d
O-Vanessa-R <vanessa.rolland@oclock.school>
2024-11-08 17:32:48 +0100

    fix(navbar): Ajout du typecomposant de navbar et modification css

--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -2,43 +2,43 @@ import { NavLink } from "react-router-dom";
 import { PAGE_LOGIN, PAGE_REGIONS, PAGE_REGISTER } from "../../App";
 
 const Navbar = ({
-	toggleMenu,
-	isMenuOpen,
+    toggleMenu,
+    isMenuOpen,
 }: {
-	toggleMenu: () => void;
-	isMenuOpen: boolean;
+    toggleMenu: () => void;
+    isMenuOpen: boolean;
 }) => {
-	return (
-		<nav
-			className={
-				isMenuOpen
-					? "navbar flex flex-col gap-2 absolute w-fit top-full left-0 right-0 bg-white shadow-md border-t-2 border-[#F5F5F5] transition p-2"
-					: "navbar flex flex-col gap-2 absolute w-fit top-full left-0 right-0 bg-white shadow-md border-t-2 border-[#F5F5F5] transition p-2 -translate-x-[102%]"
-			}
-		>
-			<NavLink
-				to={PAGE_REGIONS}
-				onClick={toggleMenu}
-				className="p-2 w-full text-left text-blueText font-sans uppercase text-lg font-semibold tracking-wide"
-			>
-				Régions
-			</NavLink>
-			<NavLink
-				to={PAGE_LOGIN}
-				onClick={toggleMenu}
-				className="p-2 w-full text-left text-blueText font-sans uppercase text-lg font-semibold tracking-wide"
-			>
-				Se connecter
-			</NavLink>
-			<NavLink
-				to={PAGE_REGISTER}
-				onClick={toggleMenu}
-				className="p-2 w-full text-left text-blueText font-sans uppercase text-lg font-semibold tracking-wide"
-			>
-				Créer son compte
-			</NavLink>
-		</nav>
-	);
+    return (
+        <nav
+            className={
+                isMenuOpen
+                    ? "navbar flex flex-col gap-2 absolute w-fit top-full left-0 right-0 bg-white shadow-md border-t-2 border-[#F5F5F5] transition p-2"
+                    : "navbar flex flex-col gap-2 absolute w-fit top-full left-0 right-0 bg-white shadow-md border-t-2 border-[#F5F5F5] transition p-2 -translate-x-[102%]"
+            }
+        >
+            <NavLink
+                to={PAGE_REGIONS}
+                onClick={toggleMenu}
+                className="p-2 w-full text-left text-blueText font-sans uppercase text-lg font-semibold tracking-wide"
+            >
+                Régions
+            </NavLink>
+            <NavLink
+                to={PAGE_LOGIN}
+                onClick={toggleMenu}
+                className="p-2 w-full text-left text-blueText font-sans uppercase text-lg font-semibold tracking-wide"
+            >
+                Se connecter
+            </NavLink>
+            <NavLink
+                to={PAGE_REGISTER}
+                onClick={toggleMenu}
+                className="p-2 w-full text-left text-blueText font-sans uppercase text-lg font-semibold tracking-wide"
+            >
+                Créer son compte
+            </NavLink>
+        </nav>
+    );
 };
 
 export default Navbar;
--- frontend/src/components/ui/NavbarDesktop.tsx
+++ frontend/src/components/ui/NavbarDesktop.tsx
@@ -1,14 +1,15 @@
 import { NavLink } from "react-router-dom";
 import { PAGE_LOGIN, PAGE_REGIONS, PAGE_REGISTER } from "../../App";
+import React from "react";
 
-const NavbarDesktop = () => {
+const NavbarDesktop: React.FC = () => {
 	return (
 		<nav className="navbar flex items-center gap-8 w-full px-6 h-full">
 			<NavLink
 				to={PAGE_REGIONS}
 				className={({ isActive }) => {
 					return isActive
-						? " text-blueText font-sans uppercase text-lg font-medium tracking-wide border-b-2 border-transparent text-green hover:border-y-blueText"
+						? " text-green font-sans uppercase text-lg font-medium tracking-wide border-b-2 border-transparent hover:border-y-blueText"
 						: " text-blueText font-sans uppercase text-lg font-medium tracking-wide border-b-2 border-transparent hover:border-y-blueText";
 				}}
 			>
@@ -18,7 +19,7 @@ const NavbarDesktop = () => {
 				to={PAGE_LOGIN}
 				className={({ isActive }) => {
 					return isActive
-						? " text-blueText font-sans uppercase text-lg font-medium tracking-wide border-b-2 border-transparent text-green hover:border-y-blueText"
+						? " text-green font-sans uppercase text-lg font-medium tracking-wide border-b-2 border-transparent hover:border-y-blueText"
 						: " text-blueText font-sans uppercase text-lg font-medium tracking-wide border-b-2 border-transparent hover:border-y-blueText";
 				}}
 			>
@@ -28,7 +29,7 @@ const NavbarDesktop = () => {
 				to={PAGE_REGISTER}
 				className={({ isActive }) => {
 					return isActive
-						? " text-blueText font-sans uppercase text-lg font-medium tracking-wide border-b-2 border-transparent text-green hover:border-y-blueText"
+						? " text-green font-sans uppercase text-lg font-medium tracking-wide border-b-2 border-transparent hover:border-y-blueText"
 						: " text-blueText font-sans uppercase text-lg font-medium tracking-wide border-b-2 border-transparent hover:border-y-blueText";
 				}}
 			>
