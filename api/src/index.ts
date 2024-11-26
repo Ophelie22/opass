5edf845e4ad406cff4c2f157a25a9b20a555fc35
Ophelie22 <laly999@hotmail.com>
2024-11-26 15:23:11 +0100

    modif index

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,38 +1,29 @@
 import express, { Request, Response } from 'express';
-import userRouter from './routers/user.router';
-import siteUserRouter from './routers/siteUser.router';
-import sitePackageRouter from './routers/sitePackage.router';
-import siteCategoryRouter from './routers/siteCategory.router';
-import siteRouter from './routers/site.router';
-import regionRouter from './routers/region.router';
-import passRouter from './routers/pass.router';
-import packageRouter from './routers/package.router';
-import orderRouter from './routers/order.router';
-// Test pour le seed
-import '../prisma/seed';
+import bodyParser from 'body-parser';
+//import authController from './auth/auth.routes';
+import dotenv from 'dotenv';
 
+const cookieParser = require('cookie-parser');
 const app = express();
+//exports.app = app;
+
+// Chargement des variables d'environnement
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+// Partie Middleware
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+// Routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world !');
 });
 
-// Middleware pour parser le JSON
-app.use(express.json());
+//app.use('/auth', authController);
 
-// Routes des 9 tables V.1
-app.use("/api/users", userRouter);
-app.use("/api/siteUser", siteUserRouter);
-app.use("/api/sitePackage", sitePackageRouter);
-app.use("/api/siteCategory", siteCategoryRouter);
-app.use("/api/site", siteRouter );
-app.use("/api/region", regionRouter);
-app.use("/api/pass", passRouter);
-app.use("/api/package", packageRouter);
-app.use("/api/order", orderRouter);
-
-app.listen(PORT, () => {
+// Démarrage du serveur
+app.listen(3000, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
