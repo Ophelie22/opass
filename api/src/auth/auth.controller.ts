ea8284ec49b6812f2ef218723f916b457a3676d8
Ophelie22 <laly999@hotmail.com>
2024-11-27 05:06:59 +0100

    fix: enlevement package.json

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import AuthService from "./auth.service";
 
-//Pour l'authentification il faut alle sur `/api/auth/register` et `/api/auth/login`.
+//Pour l'authentification il faut aller sur `/api/auth/register` et `/api/auth/login`.
 class AuthController {
   static async login(req: Request, res: Response): Promise<void> {
     try {
