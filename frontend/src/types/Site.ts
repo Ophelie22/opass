eac2aef41bc4733503009a4109219da2911f935c
O-Vanessa-R <vanessa.rolland@oclock.school>
2024-11-27 23:13:30 +0100

    feat: Ajout des fichiers de types

---
+++ frontend/src/types/Site.ts
@@ -0,0 +1,7 @@
+export interface Site {
+    id: string;
+    region_id: string;
+    category_id: string;
+    name: string;
+    media: string;
+}
---
+++ frontend/src/types/SiteCategory.ts
@@ -0,0 +1,6 @@
+export interface SiteCategory {
+    id: string;
+    name: string,
+    created_at: string;
+    updated_at: string;
+}
